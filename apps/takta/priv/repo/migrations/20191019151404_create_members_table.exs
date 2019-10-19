127e3c64407164c065d9575da0000901b03641a1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-10-19 17:48:25 +0200

    Add missing fields in migration

--- apps/takta/priv/repo/migrations/20191019151404_create_members_table.exs
+++ apps/takta/priv/repo/migrations/20191019151404_create_members_table.exs
@@ -4,6 +4,8 @@ defmodule Takta.Repo.Migrations.CreateMembersTable do
   def change do
     create table(:members, primary_key: false) do
       add :id, :uuid, primary_key: true
+      add :can_annotate, :boolean, default: false, null: false
+      add :can_comment, :boolean, default: false, null: false
       add :member_id, references(:users, on_delete: :delete_all, type: :binary_id)
       add :whiteboard_id, references(:whiteboards, on_delete: :delete_all, type: :binary_id)
 
