41ea3bf56100aab33565405bf6626b2c57c4da85
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-10-19 14:05:21 +0200

    Add empty modules later to fill in

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,4 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of Mix.Config.
-#
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
-use Mix.Config
+import Config
 
 # Configure Mix tasks and generators
 config :takta,
