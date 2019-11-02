512c54c7b157e35976d6a2780e8a875560dced59
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-02 20:50:13 +0100

    Move fixtures under test/support/data and adjust mix file

--- apps/takta/mix.exs
+++ apps/takta/mix.exs
@@ -29,7 +29,7 @@ defmodule Takta.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib", "test/support/data"]
 
   # Specifies your project dependencies.
   #
