6e4abf33c4907933809b8fbd15e89386bbda294f
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-03 22:36:54 +0100

    Remove copy-paste field in migration

--- apps/takta/priv/repo/migrations/20191101221540_create_comments_table.exs
+++ apps/takta/priv/repo/migrations/20191101221540_create_comments_table.exs
@@ -5,7 +5,6 @@ defmodule Takta.Repo.Migrations.CreateCommentsTable do
     create table(:comments, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :content, :string
-      add :code, :string
 
       add :author_id, references(:users, on_delete: :delete_all, type: :binary_id)
 
