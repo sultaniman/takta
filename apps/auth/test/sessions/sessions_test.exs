037081da9b91f529edd79f20df6f6ece2c3206fa
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-07 15:49:31 +0100

    Add tests for token validation

--- apps/auth/test/sessions/sessions_test.exs
+++ apps/auth/test/sessions/sessions_test.exs
@@ -1,5 +1,6 @@
 defmodule Auth.SessionsTest do
-  # @invalid_token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  @invalid_token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
   use Auth.DataCase
   use Auth.Query
   alias Auth.Sessions
@@ -7,6 +8,15 @@ defmodule Auth.SessionsTest do
   describe "sessions 🍪 ::" do
     test "find all_tokens works as expected" do
       assert Sessions.all_tokens() |> length() > 0
+    end
+
+    test "is_valid? token works as expected" do
+      token = Sessions.all_tokens() |> List.first()
+      assert Sessions.is_valid?(token.token)
+    end
+
+    test "is_valid? token works as expected if token is invalid or malformed" do
+      refute Sessions.is_valid?(@invalid_token)
     end
   end
 end
