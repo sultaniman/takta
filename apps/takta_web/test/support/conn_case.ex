6c08ad56d78d49d742ee1924c5b98309ae41c755
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-16 20:38:27 +0100

    Explicitly checkout auth repo in takta web as well

--- apps/takta_web/test/support/conn_case.ex
+++ apps/takta_web/test/support/conn_case.ex
@@ -28,9 +28,11 @@ defmodule TaktaWeb.ConnCase do
 
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Takta.Repo)
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Auth.Repo)
 
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(Takta.Repo, {:shared, self()})
+      Ecto.Adapters.SQL.Sandbox.mode(Auth.Repo, {:shared, self()})
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
