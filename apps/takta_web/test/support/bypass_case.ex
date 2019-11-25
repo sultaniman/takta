d468e5a35fb8692da375aaa461abb646dd8474c8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-25 16:43:03 +0100

    Create bypass test helpers

---
+++ apps/takta_web/test/support/bypass_case.ex
@@ -0,0 +1,17 @@
+defmodule TaktaWeb.BypassCase do
+  def start_bypass(_) do
+    bypass = Bypass.open
+    [bypass: bypass]
+  end
+
+  def exaws_config_for_bypass(bypass) do
+    ExAws.Config.new(:s3, [
+      access_key_id: "AKIAIOSFODNN7EXAMPLE",
+      secret_access_key: "wJalrXUtnFEMI/YYMMENG/bPxSpiCYEXAMPLEKEY",
+      host: "localhost",
+      port: bypass.port,
+      scheme: "http://",
+      region: "eu-central-1",
+    ])
+  end
+end
