f66dbc28b97615d4f364d952f3d5940c44b8f6fe
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-26 22:04:54 +0100

    Move uploaders from tests to lib

---
+++ apps/takta_web/lib/takta_web/uploaders/s3.ex
@@ -0,0 +1,9 @@
+defmodule TaktaWeb.Uploaders.S3 do
+  @moduledoc false
+  @behaviour TaktaWeb.Uploaders
+
+  @impl true
+  def upload(filename, data) do
+
+  end
+end
---
+++ apps/takta_web/test/takta_web/controllers/auth_controller_test.ex
@@ -0,0 +1,11 @@
+defmodule TaktaWeb.AuthControllerTest do
+  use TaktaWeb.ConnCase, async: true
+  alias Auth.{MagicTokens, Sessions}
+  alias Takta.Accounts
+
+  describe "auth controller 🍻 ::" do
+    test "can authenticate using social auth provider", %{conn: conn} do
+
+    end
+  end
+end
