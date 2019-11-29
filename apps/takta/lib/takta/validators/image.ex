5587925919965f98d597c270f0558901546aee92
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-29 15:47:19 +0100

    Create image validator

--- apps/takta/lib/takta/validators/image.ex
+++ apps/takta/lib/takta/validators/image.ex
@@ -6,8 +6,21 @@ defmodule Takta.Image do
 
     * PNG,
     * JPG
+
+  Usage:
+
+  ```
+    binary_image
+    |> Takta.Image.extension()
+    |> Takta.Image.is_valid?()
+  ```
   """
+  @allowed_extensions [".png", ".jpg"]
+
   def extension(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>), do: ".png"
   def extension(<<0xff, 0xD8, _::binary>>), do: ".jpg"
-  def extension(_), do: :invalid_format
+  def extension(_), do: nil
+
+  def is_valid?(nil), do: false
+  def is_valid?(ext), do: @allowed_extensions |> Enum.member?(ext)
 end
