e287057317d0fc71849f80034ed648e2a2fe1112
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-29 16:00:52 +0100

    Add AWS configuration

--- config/config.exs
+++ config/config.exs
@@ -87,6 +87,11 @@ config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
   client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
+# AWS
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
