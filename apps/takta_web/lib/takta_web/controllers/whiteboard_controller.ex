7747389f5b839a56eaa1a1dbf0cec35a37dfa7dc
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-29 15:48:15 +0100

    Cleanup whiteboard controller

--- apps/takta_web/lib/takta_web/controllers/whiteboard_controller.ex
+++ apps/takta_web/lib/takta_web/controllers/whiteboard_controller.ex
@@ -1,28 +1,13 @@
 defmodule TaktaWeb.WhiteboardController do
   @moduledoc false
   use TaktaWeb, :controller
-  alias Takta.Whiteboards
-  alias TaktaWeb.Uploaders.S3
+  alias TaktaWeb.Services.UploadService
 
-  @uploader Application.get_env(:takta_web, :uploader)
+  plug TaktaWeb.Plugs.AuthContext
+  plug TaktaWeb.Plugs.AuthRequired
 
-  def create(%Plug.Conn{assigns: %{user: user}} = conn, %{"filename" => filename, "data" => data}) do
-    # TODO: save file, get path, create whiteboard
-    case @uploader.upload(filename, data) do
-      {:ok, path} ->
-        {:ok, whiteboard} = Whiteboards.create(%{
-          name: filename,
-          path: path,
-          owner_id: user.id
-        })
-
-        conn
-        |> render("whiteboard.json", whiteboard)
-
-      {:error, reason} ->
-        conn
-        |> put_status(400)
-        |> json(%{error: reason})
-    end
+  # def create() do
+  def create(conn, params) do
+    UploadService.handle_upload(conn, params)
   end
 end
---
+++ apps/takta_web/lib/takta_web/services/upload_service.ex
@@ -0,0 +1,75 @@
+defmodule TaktaWeb.Services.UploadService do
+  @moduledoc false
+  import Plug.Conn, only: [put_status: 2]
+  import Phoenix.Controller, only: [json: 2, render: 3]
+  import Takta.Image
+
+  require Logger
+
+  alias Takta.Whiteboards
+
+  @uploader Application.get_env(:takta_web, :uploader)
+  @upload_to Application.get_env(:takta_web, :upload_to)
+
+  # TODO: get user from conn
+  @doc """
+  Handle upload and return response.
+  Depending on environemnt takes responsibility to
+
+    1. handle upload backends,
+    2. validate image format,
+  """
+  def handle_upload(
+    %Plug.Conn{assigns: %{user: user}} = conn,
+    %{"filename" => filename, "data" => data}
+  ) do
+    image = Base.decode64!(data)
+
+    case make_name(image) do
+      nil ->
+        conn
+        |> put_status(400)
+        |> json(%{error: :invalid_format})
+
+      path ->
+        case @uploader.upload(path, image) do
+          {:ok, path} ->
+            params = %{name: filename, path: path, owner_id: user.id}
+            create_whiteboard(conn, params)
+
+          {:error, reason} ->
+            conn
+            |> put_status(400)
+            |> json(%{error: reason})
+        end
+    end
+  end
+
+  defp create_whiteboard(conn, params) do
+    case Whiteboards.create(params) do
+      {:ok, whiteboard} ->
+        conn
+        |> render("whiteboard.json", whiteboard)
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        errors = %{
+          error: :validation,
+          details: Takta.Util.Changeset.errors_to_json(changeset)
+        }
+
+        conn
+        |> put_status(400)
+        |> json(errors)
+    end
+  end
+
+  defp make_name(binary_image) do
+    ext = extension(binary_image)
+
+    if is_valid?(ext) do
+      [@upload_to, "#{UUID.uuid4()}#{ext}"] |> Path.join()
+    else
+      nil
+    end
+  end
+end
