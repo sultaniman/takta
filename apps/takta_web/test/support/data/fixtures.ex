9dbf84052dbcffd0d2221fbf3074d1ac1faa4bd2
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-06 16:12:33 +0100

    Add fixtures for web

--- apps/takta_web/test/support/data/fixtures.ex
+++ apps/takta_web/test/support/data/fixtures.ex
@@ -1,7 +1,7 @@
 defmodule TaktaWeb.Fixtures do
   @moduledoc false
   use Takta.Query
-  alias Takta.Accounts
+  alias Takta.{Accounts, Whiteboards}
 
   def run do
     Accounts.create(%{
@@ -11,6 +11,21 @@ defmodule TaktaWeb.Fixtures do
       is_active: true,
       is_admin: false,
       provider: "github"
+    })
+
+    {:ok, user} = Accounts.create(%{
+      email: "web-2@example.com",
+      full_name: "Web 2 name",
+      password: "12345678",
+      is_active: true,
+      is_admin: false,
+      provider: "github"
+    })
+
+    Whiteboards.create(%{
+      name: "WB 1 web-2",
+      path: "/path/web-2.png",
+      owner_id: user.id
     })
   end
 end
