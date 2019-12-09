62a1b20e8fa5f75cab92408de359bf7042ed4efc
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-09 16:51:21 +0100

    Create initial controller with actions for comments

---
+++ apps/takta_web/lib/takta_web/comments/comment_controller.ex
@@ -0,0 +1,17 @@
+defmodule TaktaWeb.CommentController do
+  @moduledoc false
+  use TaktaWeb, :controller
+  alias TaktaWeb.Base.StatusResponse
+
+  def create(conn, _params) do
+    conn |> StatusResponse.send_response(StatusResponse.ok())
+  end
+
+  def update(conn, _params) do
+    conn |> StatusResponse.send_response(StatusResponse.ok())
+  end
+
+  def delete(conn, _params) do
+    conn |> StatusResponse.send_response(StatusResponse.ok())
+  end
+end
