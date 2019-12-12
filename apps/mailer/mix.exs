0ac69a62d15fbe4b88489f45bf3e0ad50bd2d993
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-12 23:17:10 +0100

    Move mailer app under apps

--- apps/mailer/mix.exs
+++ apps/mailer/mix.exs
@@ -5,6 +5,10 @@ defmodule Mailer.MixProject do
     [
       app: :mailer,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,7 +27,8 @@ defmodule Mailer.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end
