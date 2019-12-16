fb88c2d8c9c6f31c79d3d5e817e1632ea28b758a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-16 13:54:34 +0100

    Add an alias ecto.recreate

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule Takta.Umbrella.MixProject do
   use Mix.Project
 
   @deps []
+  @aliases [
+    "ecto.recreate": ["ecto.drop", "ecto.create", "ecto.migrate"],
+  ]
 
   def project do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: @deps
+      deps: @deps,
+      aliases: @aliases
     ]
   end
 end
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 clear;
 export MIX_ENV=test;
+mix ecto.recreate
 mix test --trace --color
 export MIX_ENV=dev;
