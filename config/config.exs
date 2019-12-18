56b76c2d9686b3c07a054949a1052a7a955f022a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-18 14:26:43 +0100

    Fix typos in configuration

--- config/config.exs
+++ config/config.exs
@@ -61,10 +61,9 @@ config :mailer, Mailer,
   adapter: Bamboo.SMTPAdapter,
   server: "smtp.gmail.com",
   port: 587,
-  username: System.get_env("EMAIL_USERNAME")
+  username: System.get_env("EMAIL_USERNAME"),
   password: System.get_env("EMAIL_PASSWORD"),
   tls: :if_available,
-  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"],
   retries: 3,
   auth: :if_available
 
