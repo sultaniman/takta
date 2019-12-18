3e5d518725b2da97bd03b998ec5dcda6aa35c62b
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-12-18 16:24:54 +0100

    Create mailer to send magic signing links

---
+++ apps/mailer/lib/mailer/magic_link.ex
@@ -0,0 +1,32 @@
+defmodule Mailer.MagicLink do
+  @moduledoc false
+  import Bamboo.Email
+  require Logger
+
+  @doc """
+  Send magic link to requested email
+  so later user can sign in using it.
+  """
+  def new_link(email, link) do
+    Logger.info("Send login link #{link}")
+
+    body = ~s(
+      Hey,
+
+      Please us the following link to sing in to your account
+
+      #{link}
+
+      Thanks.
+    )
+
+    new_email(
+      to: email,
+      from: Application.get_env(:mailer, :from_email),
+      subject: "Login link",
+      text_body: body
+    )
+  end
+
+  def schedule_delivery(message), do: message |> Mailer.deliver_later()
+end
